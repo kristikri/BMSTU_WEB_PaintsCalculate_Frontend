81bbabba1e06782ce30cf16454169b89ac5fc572
kristikri <kborisova05@mail.ru>
2025-11-10 23:52:17 +0300

    Create lab6

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   server: {
@@ -16,5 +17,32 @@ export default defineConfig({
     host: true, // нужно, чтобы правильно работал маппинг портов в docker-контейнере
     strictPort: true, // необязательно
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true,
+    },
+    manifest:{
+      name: "Tile Notes",
+      short_name: "Tile Notes",
+      start_url: "/",
+      display: "standalone",
+      background_color: "#fdfdfd",
+      theme_color: "#db4938",
+      orientation: "portrait-primary",
+      icons: [
+        {
+          "src": "/logo192.png",
+          "type": "image/png", "sizes": "192x192"
+        },
+        {
+          "src": "/logo512.png",
+          "type": "image/png", "sizes": "512x512"
+        }
+      ],
+    }
+    })
+  ],
 })
